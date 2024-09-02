92913937030d86c28be17f298163633d2365112b
Donald <gugadonald10@gmail.com>
2024-09-02 22:18:52 +0200

    Front-End

--- palestra-main/palestra/src/main/java/com/generation/palestra/controllers/AppController.java
+++ palestra-main/palestra/src/main/java/com/generation/palestra/controllers/AppController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class AppController 
 {
     
-    @GetMapping("/")
+    @GetMapping("/index")
     public String root()
     {
         return "index.html";
@@ -17,6 +17,42 @@ public class AppController
     public String registrazione()
     {
         return "registrati.html";
+    }
+
+    @GetMapping("/chisiamo")
+    public String chisiamo()
+    {
+        return "chiSiamo.html";
+    }
+
+    @GetMapping("/recensioni")
+    public String recensione()
+    {
+        return "recensione.html";
+    }
+
+    @GetMapping("/abbonamenti")
+    public String abbonamenti()
+    {
+        return "abbonamenti.html";
+    }
+
+    @GetMapping("/corsi")
+    public String corsi()
+    {
+        return "corsi.html";
+    }
+
+    @GetMapping("/privacy")
+    public String privacy()
+    {
+        return "privacy.html";
+    }
+    
+    @GetMapping("/terminiecondizioni")
+    public String terminiecondizioni()
+    {
+        return "terminiecondizioni.html";
     }
 
 }
